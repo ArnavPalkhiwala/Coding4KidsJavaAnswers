b0b7b4257e290f1a86dc9cae0cc120701d5772ec
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2020-07-22 23:35:10 -0700

    Finished Week 2

---
+++ WeekTwo.java
@@ -0,0 +1,49 @@
+public class WeekTwo {
+	
+	//Review Practice One
+	
+//	public static void main(String[] args) {
+//		int x = 10;
+//		int y = 15;
+//		System.out.println(x + y);
+//	}
+//	
+	//Review Practice Two
+	
+//	public static void main(String[] args) {
+//		int age = 15;
+//		String name = "Arnav";
+//		System.out.println("My name is " + name + " and I am " + age + " years old");
+//	}
+	
+	//If/Else If/Else Statement Practice
+	
+//	public static void main(String[] args) {
+//		int x = 80;
+//		int y = 18;
+//		
+//		if (x > y) {
+//			System.out.println("X is the larger number");
+//		}
+//		
+//		else if(y > x) {
+//			System.out.println("Y is the larger number");
+//		}
+//		
+//		else {
+//			System.out.println("X and Y are equal");
+//		}
+//	}
+	
+	//While Loop Practice
+//	
+//	public static void main(String[] args) {
+//	int x = 10;
+//	int y = 15;
+//	while (y > x) {
+//		System.out.println(x);
+//		x += 1;
+//	}
+//	}
+
+}
