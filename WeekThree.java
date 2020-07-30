5ef87961dc3c4e12c81f3a166cb697bee5427b5c
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2020-07-30 00:20:30 -0700

    Week 3

---
+++ WeekThree.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+public class WeekThree {
+	
+	public static void main(String[] args) {
+		
+//Review Practice 1
+//		int x = 10;
+//		int y = 15;
+//		if (x > y) {
+//			System.out.println("x>y");
+//		}
+//		else if (y > x) {
+//			System.out.println("y>x");
+//		}
+//		else {
+//			System.out.println("x and y are =");
+//		}
+//		
+
+//Review Practice 2
+//		
+//		String favoriteColor = "blue";
+//		String name = "Arnav";
+//		
+//		System.out.println("My name is " + name + " and my favorite color is " + favoriteColor);
+		
+//For loop Practice
+//		
+//		for (int i = 0; i < 6; i++) {
+//			System.out.println(i * 10);
+//		}
+		
+//For Loop Practice 2
+		
+//		for (int i = 0; i < 5; i++) {
+//			
+//			System.out.println(i - 10);
+//			if (i > 2) {
+//				System.out.println("i is greater than 2");
+//			}
+//			
+//			else {
+//				System.out.println("i is less than or equal to 2");
+//			}
+//			
+//		}
+		
+		
+//Multiple Situations Practice
+//		
+//		int x = 10;
+//		String color = "blue";
+//		
+//		if (x > 100 && color.contains("a")) {
+//			System.out.println("x>100 and color contains a");
+//		}
+//		
+//		else {
+//			System.out.println("x <= 100 and color does not contain a");
+//		}
+		
+//Random Practice
+//		
+//		Random r = new Random();
+//		int rand = r.nextInt(3);
+//		System.out.println("The random number is " + rand);
+//		if (rand == 0 || rand == 1) {
+//			System.out.println("Random is 0 or 1");
+//		}
+//		else {
+//			System.out.println("Random = 2");
+//		}
+//		
+//		
+	}
+
+}
