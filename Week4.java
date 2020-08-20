7e48be838013aa71937524bab6a43e264ead9d7c
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2020-08-19 17:24:11 -0700

    Add files via upload

---
+++ Week4.java
@@ -0,0 +1,75 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Week4 {
+
+	public static void main(String[] args) {
+		
+		//Review Practice 1
+		
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("Name");
+//			System.out.println(i);
+//		}
+		
+		//Review Practice 2
+//		for(int i = 1; i < 10; i+=2) {
+//			Random r = new Random();
+//			int rand = r.nextInt(i);
+//			System.out.println(rand);
+//		}
+		
+		//User Input Practice
+//		
+//		String name = JOptionPane.showInputDialog("What is your name?");
+//		String color = JOptionPane.showInputDialog("What is your favorite color?");
+//		
+//		System.out.println("Your name is " + name + " and your favorite color is " + color);
+		
+		//User Input with Numbers Practice
+//		
+//		String number = JOptionPane.showInputDialog("Type a number between 0 - 10");
+//		int num = Integer.parseInt(number);
+//		
+//		Random r = new Random();
+//		int ran = r.nextInt(10);
+//		
+//		if(num == ran) {
+//			System.out.println("Good Job!");
+//		}
+//		
+//		else {
+//			System.out.println("Incorrect");
+//		}
+		
+		//Pop Up Messages Practice
+//		
+//		String team = JOptionPane.showInputDialog("What is your favorite sports team?");
+//		String num = JOptionPane.showInputDialog("What is your favorite number?");
+//		int number = Integer.parseInt(num);
+//		
+//		JOptionPane.showMessageDialog(null, "Your favorite sports team is " + team + " and your favorite number is " + number);
+
+		//Putting it all Together
+		
+//		String number = JOptionPane.showInputDialog("Enter a number between 0 - 100");
+//		int num = Integer.parseInt(number);
+//		
+//		if(num > 0 && num <= 25) {
+//			JOptionPane.showMessageDialog(null, "You will win something");
+//		}
+//		
+//		else if(num > 25 && num <= 50) {
+//			JOptionPane.showMessageDialog(null, "You will get some money");
+//		}
+//		
+//		else if(num > 50 && num <= 75) {
+//			JOptionPane.showMessageDialog(null, "Something unfortunate will happen");
+//		}
+//		
+//		else {
+//			JOptionPane.showMessageDialog(null, "I could not find a fortune for you");
+//		}
+	}
+}
