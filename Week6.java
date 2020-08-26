38e59b444153e0ab3a113bcdb57bf46c155d43f3
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2020-08-25 18:39:58 -0700

    Add files via upload

---
+++ Week6.java
@@ -0,0 +1,34 @@
+package yoyhp;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Week6 {
+
+//	public static void main(String[] args) {
+//		
+//		
+//		Week6 w = new Week6();
+//		w.nameAndAge("bob", 100);
+//	}
+//	
+//	void nameAndAge(String name, int age) {
+//		System.out.println("Your name is " + name + " and your age is " + age);
+//	}
+//
+
+//	double run() {
+//		return 2.5;
+//	}
+//	
+//	double run2() {
+//		return 2.5;
+//	}
+
+	public static void main(String[] args) {
+		System.out.println(Math.pow(10, 2));
+		System.out.println(Math.sqrt(100));
+	}
+
+}
